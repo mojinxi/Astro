cfc993d6f870ae5e5080c06ca61a1b3f8019689b
mojinxi <42954527+mojinxi@users.noreply.github.com>
2026-06-06 01:57:14 +0800

    简化配置，移除错误变量

--- astro.config.ts
+++ astro.config.ts
@@ -1,34 +1,7 @@
 import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
-import sitemap from '@astrojs/sitemap';
-import compress from 'astro-compress';
-import rehautolinkheadings from 'rehype-autolink-headings';
-import rehypeSlug from 'rehype-slug';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://astro.pages.dev', // 你的博客线上地址，可以暂时留空或改成你的 Cloudflare 域名
-  integrations: [
-    tailwind({
-      applyBaseStyles: false,
-    }),
-    sitemap(),
-    compress(),
-  ],
-  markdown: {
-    rehypePlugins: [
-      rehypeSlug,
-      [
-        rehautolinkheadings,
-        {
-          behavior: 'wrap',
-          properties: { class: 'anchor' },
-        },
-      ],
-    ],
-  },
+  site: 'https://astro.pages.dev',
   output: 'static',
-  build: {
-    inlineStylesheets: 'auto',
-  },
 });
